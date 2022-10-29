f1eab52ab7e69e0b8c397c8de86bb3c2ceb9d80a
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-10-29 16:57:06 +0200

    introduce yaml-cpp to load parameters from file

--- include/mpc_params.hpp
+++ include/mpc_params.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdexcept>
+#include <iostream>
+#include <yaml-cpp/yaml.h>
 
 struct MPCParams 
 {
@@ -9,23 +11,46 @@ struct MPCParams
     int nthreads = 6;
     int iter = 1;
     int sim_steps_per_mpc_update = 1;
-};
 
-void checkMPCParams(const MPCParams& mpc_params) 
-{
-    if (mpc_params.T <= 0.0) {
-        throw std::runtime_error("MPCParams.T must be positive!");
+    void loadFromYAML(const YAML::Node& config) {
+        if (config["T"].IsDefined()) 
+            T = config["T"].as<double>();
+        if (config["N"].IsDefined()) 
+            N = config["N"].as<int>();
+        if (config["iter"].IsDefined()) 
+            iter = config["iter"].as<int>();
+        if (config["nthreads"].IsDefined()) 
+            nthreads = config["nthreads"].as<int>();
+        if (config["sim_steps_per_mpc_update"].IsDefined()) 
+            sim_steps_per_mpc_update = config["sim_steps_per_mpc_update"].as<int>();
     }
-    if (mpc_params.N < 0.0) {
-        throw std::runtime_error("MPCParams.N must be positive!");
+
+    void check() const {
+        if (T <= 0.0) {
+            throw std::invalid_argument("MPCParams.T must be positive!");
+        }
+        if (N < 0.0) {
+            throw std::invalid_argument("MPCParams.N must be positive!");
+        }
+        if (nthreads < 0.0) {
+            throw std::invalid_argument("MPCParams.nthreads must be positive!");
+        }
+        if (iter < 0.0) {
+            throw std::invalid_argument("MPCParams.iter must be positive!");
+        }
+        if (sim_steps_per_mpc_update < 0.0) {
+            throw std::invalid_argument("MPCParams.sim_steps_per_mpc_update must be positive!");
+        }
     }
-    if (mpc_params.nthreads < 0.0) {
-        throw std::runtime_error("MPCParams.nthreads must be positive!");
+
+    friend std::ostream& operator<<(std::ostream& os, 
+                                    const MPCParams& mpc_params) {
+        os << "MPCParams:" << "\n";
+        os << "  T:                          " << mpc_params.T << "\n";
+        os << "  N:                          " << mpc_params.N << "\n";
+        os << "  nthreads:                   " << mpc_params.nthreads << "\n";
+        os << "  iter:                       " << mpc_params.iter << "\n";
+        os << "  sim_steps_per_mpc_update:   " << mpc_params.sim_steps_per_mpc_update << "\n";
+        return os;
     }
-    if (mpc_params.iter < 0.0) {
-        throw std::runtime_error("MPCParams.iter must be positive!");
-    }
-    if (mpc_params.sim_steps_per_mpc_update < 0.0) {
-        throw std::runtime_error("MPCParams.sim_steps_per_mpc_update must be positive!");
-    }
-}
+};
