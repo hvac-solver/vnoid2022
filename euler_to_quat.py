0e19b0f4c3d1d04820f59529dfb2c5c4a12db8e1
Sotaro Katayama <33686357+mayataka@users.noreply.github.com>
2022-11-11 14:09:19 +0100

    Delete euler_to_quat.py

--- euler_to_quat.py
+++
@@ -1,8 +0,0 @@
-from imageio import imopen
-from scipy.spatial.transform import Rotation as R
-
-r = R.from_euler('zyx', angles=[0.3, 0.5, 0.0], degrees=False).as_quat()
-print(r)
-
-r = R.from_euler('zyx', angles=[-0.3, 0.5, 0.0], degrees=False).as_quat()
-print(r)
